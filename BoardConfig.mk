02d6a709ab1ff39944d4146da9117f7f3e23145b
Nolen Johnson <johnsonnolen@gmail.com>
2020-03-17 16:20:15 -0400

    molly: Import initial device tree
    * Pulled from CyanogenMod's skeleton device makefiles found here: https://github.com/CyanogenMod/android_build/tree/cm-14.1/tools/device.
    * Added Android.mk dictating molly as the only compatible device.
    * Populated partition sizes by pushing busybox, and then using fdisk on the device.
    * Add a lineage build target for molly, as well as the corresponding entries in the
      `vendorsetup.sh` script.
    * Populate Architechure related board flags.
    * In the Lineage product:
     * Inherit the full base product.
     * Define the build fingerprint directly instead of by build.prop override as to avoid a vendor mismatch
       error on boot.
     * Add flags to enable dynamic boot animation creation support using molly's default output resolution.
     * Inherit Lineage's common configuration for TV devices.
     * Set GMS Client ID to Google.
     * Inherit proprietary blobs (if they exist) in products, as we may have people adding new
       targets to build sans-blobs.
    
    Signed-off-by: Nolen Johnson <johnsonnolen@gmail.com>
    Change-Id: I23f181128bb11315cff0a93d82547abd85879f7e

---
+++ BoardConfig.mk
@@ -0,0 +1,45 @@
+# Copyright (C) 2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+# inherit from the proprietary version
+
+DEVICE_PATH := device/google/molly
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a15
+
+# Board
+TARGET_BOARD_PLATFORM := tegra4
+TARGET_BOOTLOADER_BOARD_NAME := molly
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Graphics
+USE_OPENGL_RENDERER := true
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_CACHEIMAGE_PARTITION_SIZE := 734003200
+BOARD_FLASH_BLOCK_SIZE := 4096
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13912506368
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Inherit proprietary configuration
+include vendor/google/molly/BoardConfigVendor.mk
