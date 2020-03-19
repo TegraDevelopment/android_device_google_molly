4c4bc15647b4f78112d116b1dcb29b7e1ac2baa3
razorloves <razorloves@gmail.com>
2020-03-18 21:08:36 -0400

    molly: Dexpreopt speed compile SystemUI
    Change-Id: I3331f8e65898f8696953b2602b9ad963f7e4313d

--- device.mk
+++ device.mk
@@ -45,6 +45,9 @@ PRODUCT_COPY_FILES += \
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
+# Dexpreopt
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
+
 # GL wrapper
 PRODUCT_PACKAGES += \
     libmhax
