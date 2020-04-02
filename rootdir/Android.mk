ec0335109a618949b831bd7e0ab2c73325ad0098
padarshr <padarshr@codeaurora.org>
2020-04-02 17:43:35 -0400

    molly: Address 'LOCAL_MODULE_TAGS := eng debug' deprecation.
    * Newer AOSP versions (AOSP 11.0) deprecate LOCAL_MODULE_TAGS := eng debug.
      We now remove that and instead add the package to
      PRODUCT_PACKAGES_ENG or PRODUCT_PACKAGES_DEBUG as applicable.
    
    * To ensure that we don't miss out on any package during this switch,
      we add these explicitly, even though it might have been already
      added under PRODUCT_PACKAGES at some other location.
    
    Change-Id: I35bd67c3e7752be5bcd8f8682864062173e92724

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.molly
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := fstab.molly
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -10,7 +10,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.molly.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.molly.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -18,7 +18,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.molly.usb.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.molly.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -26,7 +26,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.molly.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.molly.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
@@ -34,7 +34,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.molly.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := ueventd.molly.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
