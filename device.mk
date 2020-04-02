0fd51b03d977aac9621207d36f8256e8d58188f9
Nolen Johnson <johnsonnolen@gmail.com>
2020-04-01 21:23:03 -0400

    molly: Import root directory files
    * These were pulled from the latest stock release.
    
    Signed-off-by: Nolen Johnson <johnsonnolen@gmail.com>
    
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Co-authored-by: "Greg Willard (r3pwn)" <wickett06@gmail.com>
    Co-authored-by: Matt Gorski <matt.gorski@gmail.com>
    Co-authored-by: dhacker29 <dhackerdvm@gmail.com>
    Change-Id: Ife10e58c59a5ac33c22036b9ef80d20c66687303

--- device.mk
+++ device.mk
@@ -58,6 +58,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     power.molly
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.molly \
+    init.molly.rc \
+    init.molly.usb.rc \
+    ueventd.molly.rc
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
