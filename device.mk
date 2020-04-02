ec06a967dfa2fd8d5568f75a496597178ea06fec
Nolen Johnson <johnsonnolen@gmail.com>
2020-04-01 21:23:03 -0400

    molly: Import root directory files
    * These were pulled from the latest stock release's boot image and heavily organized.
    * init-scripts pruned to not include hardware we don't have/boards that we are not.
    * Organized uevent.
    * Clean out unused services and consolidate/organize as needed.
    * Also update for Android 7.0's needs.
    
    * Also import recovery init-scripts and declare recovery fstab.
    
    Signed-off-by: Nolen Johnson <johnsonnolen@gmail.com>
    
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Co-authored-by: "Greg Willard (r3pwn)" <wickett06@gmail.com>
    Co-authored-by: Matt Gorski <matt.gorski@gmail.com>
    Co-authored-by: dhacker29 <dhackerdvm@gmail.com>
    Change-Id: Ife10e58c59a5ac33c22036b9ef80d20c66687303

--- device.mk
+++ device.mk
@@ -56,6 +56,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     power.molly
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.molly \
+    init.molly.rc \
+    init.molly.usb.rc \
+    ueventd.molly.rc
+
 # System Properties
 -include $(LOCAL_PATH)/system_prop.mk
 
