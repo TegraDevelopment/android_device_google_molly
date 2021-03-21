42e28fd91842d53eee02c0b3c90e3da0b4db13ec
Nolen Johnson <johnsonnolen@gmail.com>
2021-03-21 13:06:18 -0400

    molly: Build Wi-Fi HAL
    * All of these are present on stock.
    
    Change-Id: I6e06dba35162fce3e38a010199507278e4a1d1af

--- device.mk
+++ device.mk
@@ -47,6 +47,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayouts/Spike.kl:system/usr/keylayout/Spike.kl
 
 # Wi-Fi
+PRODUCT_PACKAGES += \
+    dhcpcd.conf \
+    hostapd \
+    libwpa_client \
+    wpa_supplicant
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
